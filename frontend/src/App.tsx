e5783ce38c5862a99986262c7259f704ae0b218b
Tim Yu <yuyu.tim@gmail.com>
2025-03-28 22:17:48 -0500

    Frontend gets data from backend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [message, setMessage] = useState("No message");
+
+  // Get the message from the root of the backend.
+  useEffect(() => {
+    axios.get(process.env.REACT_APP_BACKEND_BASE_URL || "localhost://3001")
+      .then(response => setMessage(response.data))
+      .catch(error => "Error");
+  });
+
   return (
     <div className="App">
       <header className="App-header">Welcome to StressLess</header>
+      <p>{message}</p>
     </div>
   );
 }
