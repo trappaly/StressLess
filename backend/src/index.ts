b16d7fa96ddf658bfd029bdcab231f1dd36b6339
Tim Yu <yuyu.tim@gmail.com>
2025-03-30 17:26:08 -0500

    change backend port

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -8,7 +8,7 @@ const cors = require("cors");
 // Configuration
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(express.json());
