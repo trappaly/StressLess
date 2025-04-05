c0eae0582021eb4941ba597c49877b299127b90a
Alyssa Trapp <alyssatrapp@Alyssas-MacBook-Pro.local>
2025-04-05 15:30:41 -0500

    updated routes

--- backend/src/routes/preferences_routes.ts
+++
@@ -1,5 +0,0 @@
-
-import {Router} from 'express'; 
-
-const express = require("express");
-const router = express.Router();
--- backend/src/routes/user_routes.ts
+++ backend/src/routes/user_routes.ts
@@ -1,47 +1,92 @@
 import {Router} from 'express'; 
-import * as expressJwt from 'express-jwt';
+import {Request, Response, NextFunction} from 'express';
+// import * as expressJwt from 'express-jwt';
 
-
-
+// Creates the router 
 const express = require("express");
 const router = express.Router();
 
-import HomeController from '../controllers/Home';
+// Keeps track of time on website
+const timeLog = (req: Request, res: Response, next: NextFunction) => {
+  console.log('Time: ', Date.now())
+  next()
+}
+router.use(timeLog)
+
+// Login route
+router.get('/', (req: Request, res: Response) => {
+  res.send('LogIn');
+})
+
+// Sign up route 
+  router.get('/', (req: Request, res: Response) => {
+  res.send('SignUp');
+})
+
+// Authenticate with Google route
+  router.get('/', (req: Request, res: Response) => {
+  res.send('GoogleAuthentication');
+})
+
+// Authetnicate with Microsoft
+ router.get('/', (req: Request, res: Response) => {
+  res.send('MicrosfotAuthentication');
+})
+
+// Log out route 
+router.get('/', (req: Request, res: Response) => {
+  res.send('LogOut');
+})
+
+// Survey results route
+router.get('/', (req: Request, res: Response) => {
+  res.send('SurveyResults');
+})
+
+
+
+//import HomeController from '../controllers/Home';
+
+// Sign up route
+//router.get('/signup', RegisterController.show);
+//router.post('/signup', RegisterController.perform);
+
+// Google authetnication 
+// router.get('/auth/google', GoogleController.show);
+// router.post('auth/google', GoogleController.perform);
+
+// Microsoft authentication 
+// router.get('/auth/microsoft', MicrosoftController.show);
+// router.post('/auth/microsoft', MicrosoftController.perform);
+
+// Log in route 
+// router.get('/login',  LoginController.show);
+// router.post('/login', LoginController.perform);
+
+// Log out 
+// router.get('/logout', LogoutController.show);
+// router.get('/logout', LogoutController.perform);
 
 
 
 // GET method route
-app.get('/', (req, res) => {
-    res.send('GET request to the homepage')
-  })
-  
-  // POST method route
-  app.post('/', (req, res) => {
-    res.send('POST request to the homepage')
-  })
+//router.get('/', (req: Request, res: Response) => {
+  //res.send('GET request to the homepage');
+//})
 
 
-// Sign up route
-router.get('/signup', RegisterController.show);
-router.post('/signup', RegisterController.perform);
+// POST method route 
+ // router.post('/', (req: Request, res: Response) => {
+ // res.send('POST request to the homepage');
+// })
 
-// Google authetnication 
-router.get('/auth/google',  );
-router.post('auth/google');
+// PUT method route
+  // router.put('/', (req: Request, res: Response) => {
+  // res.send('PUT request to the homepage');
+// })
 
-// Microsoft authentication 
-router.get('/auth/microsoft');
-router.post('/auth/microsfot');
-
-// Log in route 
-router.get('/login',  LoginController.show);
-router.post('/login', LoginController.perform);
-
-// Log out 
-router.get('/logout', LogoutController.perform);
-
-// Save survey responses 
-router.get('/survey/save');
-router.get('/survey/save');
-
+// DELETE method route
+ // router.delete('/', (req: Request, res: Response) => {
+  // res.send('DELETE request to the homepage');
+//})
 
