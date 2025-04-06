e46cc2f49cc6c17b7b90f8a02197995f0f28ddea
Alyssa Trapp <alyssatrapp@Alyssas-MacBook-Pro.local>
2025-04-06 09:44:14 -0500

    fixed routes and edited controllers

--- backend/src/controllers/Auth/Login.ts
+++ backend/src/controllers/Auth/Login.ts
@@ -1,12 +1,7 @@
 import { Request, Response} from 'express';
 import {PrismaClient} from '@prisma/client';
 import { signInWithEmailAndPassword } from "firebase/auth";
-import { auth } from '../firebaseConfig'; // Your Firebase configuration
-
-
-
-bcrypt from 'bcrypt';
-
+// import { auth } from '../firebaseConfig'; // Your Firebase configuration
 
 
 const prisma = new PrismaClient();
@@ -18,7 +13,7 @@ class Login {
 
     public static async login (req: Request, res: Response): Promise <any > {
      // Get variables for the login process
-     const {email} = req.body;
+     const {email, password} = req.body;
      try {
          // Check if user exists
          const user = await prisma.user.findUnique({
@@ -32,8 +27,7 @@ class Login {
                  message:
                      "Invalid email or password. Please try again with the correct credentials.",
              });
-         // Valid password
-         
+
          // Returns user info except password
          const { password, ...user_data } = user._doc;
  
--- backend/src/controllers/User/Responses.ts
+++ backend/src/controllers/User/Responses.ts
@@ -3,44 +3,62 @@ import {PrismaClient} from '@prisma/client';
 
 const prisma = new PrismaClient();
 
+
 class SurveyResponse {
 
-    
     // Gets the user responses 
-    public static getResponses (req: Request, res: Response): any {
-        res.send('get Survey Responses');
-    }
-
-    // Saves the user responses to the database 
-    public static saveResponses (req: Request, res: Response): any {
-        res.send('save Survey Response');
-    }
-
-
-
-    /* const result = await prisma.user.findMany({
-        where: {
-          OR: [
-            {
-              email: {
-                endsWith: 'gmail.com',
+    public static async getResponses (req: Request, res: Response): any {
+        const surveyresponses = await prisma.user.findMany({
+            where: {
+              OR: [
+                {
+                  email: {
+                    endsWith: 'gmail.com',
+                  },
+                },
+                { email: { endsWith: 'company.com' } },
+              ],
+              NOT: {
+                email: {
+                  endsWith: 'admin.company.com',
+                },
               },
             },
-            { email: { endsWith: 'company.com' } },
-          ],
-          NOT: {
-            email: {
-              endsWith: 'admin.company.com',
+            select: {
+              email: true,
             },
-          },
-        },
-        select: {
-          email: true,
-        },
-      })
+          })
+          res.status(200).json({
+            status: "success",
+            data: surveyresponses;
+            message: "You have successfully retrieved survey responses.",
+        });
+    } catch (error: any) {
+            res.status(500).json({
+            status: "error",
+            code: 500,
+            data: [],
+            message: "Error in retrieving responses",
+        });
+    }
+        res.send('get Survey Responses');
 
-*/
+    // Saves the user responses to the database 
+    public static async saveResponses (req: Request, res: Response): any {
 
-}
-
+        res.status(200).json({
+            status: "success",
+            data: surveyresponses;
+            message: "You have successfully saved survey responses.",
+        });
+    } catch (error: any) {
+            res.status(500).json({
+            status: "error",
+            code: 500,
+            data: [],
+            message: "Error in saved survey responses",
+        });
+        res.send('save Survey Response');
+    }
+}     
 export default SurveyResponse;
