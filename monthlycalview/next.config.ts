6b2b5861b8901f1488594c4d4e2def84c12dd0bd
Cheyannne <ORGANICHEALINGG@GMAIL.COM>
2025-04-07 20:29:45 +0000

    intial push

---
+++ monthlycalview/next.config.ts
@@ -0,0 +1,16 @@
+
+
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+};
+
+export default nextConfig;
+
+
+// //edited version
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {}
+
+// module.exports = nextConfig
