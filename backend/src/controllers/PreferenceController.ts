8c18245268503e27b2c001c622c4c336659bb616
khanhdo05 <khanhphuongdo28@gmail.com>
2025-04-13 13:19:28 -0500

    fix comments

--- backend/src/controllers/PreferenceController.ts
+++ backend/src/controllers/PreferenceController.ts
@@ -1,21 +1,24 @@
-import { Request, Response } from "express";
+import { Request, Response } from 'express';
 import prisma from '../config/prisma';
 
 export default class PreferenceController {
-  public static async postPreferences (req: Request, res: Response): Promise<any> {
+  public static async postPreferences(
+    req: Request,
+    res: Response
+  ): Promise<any> {
     try {
-      // Creates variables to access the user id and variable responeses 
+      // Creates variables to access the user id and variable responses
       const userId = req.params.user_id;
       const preferences = req.body;
 
-      // New array to store responses 
+      // New array to store responses
       const survey_responses = [];
       // Loops over each user's response in the question and answer array from the user
       for (let preference of preferences) {
         // Finds each question asked on the survey and gets id
         const question = await prisma.preference_questions.findFirst({
-          where: { 
-            question_text: preference.question_text
+          where: {
+            question_text: preference.question_text,
           },
         });
         if (!question) {
@@ -28,20 +31,21 @@ export default class PreferenceController {
           answer: preference.answer,
         });
       }
-  
-      // Stores all of the user's responses in the database
+
+      // Stores all the user's responses in the database
       const stored_responses = await prisma.user_preferences.createMany({
-        // Accesses neccesary variables in user's preferences model 
+        // Accesses neccesary variables in user's preferences model
         data: survey_responses,
         // Ensures we don't save the same responses more than once in the database
         skipDuplicates: true,
       });
-      // Succuesfully is able to store survey responses in the database
+      // Successfully able to store survey responses in the database
       res.json(stored_responses);
-
     } catch (error: any) {
       // Unable to get responses
-      res.status(500).json({ error: 'Error in saving preferences, ' + error.message });
+      res
+        .status(500)
+        .json({ error: 'Error in saving preferences, ' + error.message });
     }
   }
 }
