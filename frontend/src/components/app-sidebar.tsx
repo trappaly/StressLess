42b7ad6ac11b76871b43dcaf53be055197313470
Nifemi Ogunmesa <95006475+Nogunmesa@users.noreply.github.com>
2025-04-14 19:07:33 +0000

    Draft of dashboard

---
+++ frontend/src/components/app-sidebar.tsx
@@ -0,0 +1,57 @@
+import { Calendar, UserIcon, Settings } from 'lucide-react';
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from '@/components/ui/sidebar';
+
+// Menu items.
+const items = [
+  {
+    title: 'Profile',
+    url: '#',
+    icon: UserIcon,
+  },
+  {
+    title: 'Calendar',
+    url: '#',
+    icon: Calendar,
+  },
+  {
+    title: 'Settings',
+    url: '#',
+    icon: Settings,
+  },
+];
+
+export function AppSidebar() {
+  return (
+    <Sidebar>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Application</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  );
+}
--- frontend/src/components/ui/label.tsx
+++ frontend/src/components/ui/label.tsx
@@ -20,5 +20,3 @@ function Label({
     />
   );
 }
-
-export { Label };
