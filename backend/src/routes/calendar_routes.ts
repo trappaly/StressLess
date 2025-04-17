290933becaa0b790c32722e0ec6797fff22c2087
Alyssa Trapp <trappaly@grinnell.edu>
2025-04-17 12:36:50 -0500

    Added deadline controllers and routes

--- backend/src/routes/calendar_routes.ts
+++ backend/src/routes/calendar_routes.ts
@@ -1,11 +1,11 @@
 import { Router } from 'express'; 
 import { Request, Response, NextFunction } from 'express';
 import Event from '../controllers/Calendar/Event';
-
-// import * as expressJwt from 'express-jwt';
+import Deadline from '../controllers/Calendar/Deadline';
 
 // Creates the router 
-const router = Router();
+const express = require("express");
+const router = express.Router();
 
 // Keeps track of time on website
 const timeLog = (req: Request, res: Response, next: NextFunction) => {
@@ -28,5 +28,20 @@ router.put('/events/id/:id', Event.putEvent);
 
 // Delete an event
 router.delete('/events/id/:id', Event.deleteEvent);
+
+// Get all deadlines for a particular user 
+router.get('/deadlines/by-user/:user', Deadline.getUserDeadlines);
+
+// Add a deadline
+router.post('/deadlines', Deadline.postDeadline);
+
+// Get a deadline by id
+router.get('/deadlines/id/:id', Deadline.getDeadlinebyId);
+
+// Modify a deadline 
+router.put('/deadlines/id/:id', Deadline.putDeadline);
+
+// Delete a deadline 
+router.delete('/deadlines/id/:id', Deadline.deleteDeadline);
 
 export default router;
