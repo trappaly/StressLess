e76eb2196bd7c8b55d9b09d9596dd0d2224163d5
Nifemi Ogunmesa <95006475+Nogunmesa@users.noreply.github.com>
2025-04-22 01:52:27 +0000

    Work duration and sleep amount correctly displays if answer should be in minutes or hours

--- frontend/src/app/dashboard/calendar/page.tsx
+++ frontend/src/app/dashboard/calendar/page.tsx
@@ -162,7 +162,7 @@ export default function Home() {
             className="ml-8 w-full border-2 p-2 rounded-md mt-16 lg:h-1/2 bg-violet-50"
           >
             <h1 className="font-bold text-lg text-center">Drag Event</h1>
-            
+
             {events.map((event) => (
               <div
                 className="fc-event border-2 p-1 m-2 w-full rounded-md ml-auto text-center bg-white dark:bg-black"
