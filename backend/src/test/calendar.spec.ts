7ad5b61421b46cb9dfd7b73e91b17a2961bc760d
madelds <sibalmadel@gmail.com>
2025-04-24 18:59:29 -0500

    Added put and get calendar event tests

--- backend/src/test/calendar.spec.ts
+++ backend/src/test/calendar.spec.ts
@@ -1,75 +1,184 @@
-// import { describe, it, expect, beforeAll, afterAll } from 'vitest';
-// import app from '../index';
-// import request from 'supertest';
-// import FakeDataFactory from './FakeDataFactory';
-// import prisma from '../config/prisma';
+import { describe, it, expect, beforeAll, afterAll } from 'vitest';
+import app from '../index';
+import request from 'supertest';
+import FakeDataFactory from './FakeDataFactory';
+import prisma from '../config/prisma';
 
-// describe('Test preference routes', () => {
-//     // generate fake data
-//     let factory: FakeDataFactory;
-//     // declares a user object but does not assign anything to it
-//     let user: { id: any; email?: string; created_at?: Date; };
-//     // declares a preference object but does not assign anything to it
-//     let preference: { id: any; user_id: any; question_id: any; answer: string;};
-//     // declares a preference object but does not assign anything to it
-//     let preferenceQuestion: { id: any; question_text: any};
-//     // declaring an array of preference questions
-//     let preferenceQuestions: any[] = [];
+describe('Test calendar routes', () => {
+    // generate fake data
+    let factory: FakeDataFactory;
+    // declares a user object but does not assign anything to it
+    let user: { id: any; email?: string; created_at?: Date; };
+    // declares a user event object but does not assign anything to it
+    let userEvent: { id: any; user_id: any; title: any; start_time: Date; is_recurring: any; is_generated: any; created_at: Date; };
+    // declaring an array of events
+    let userEvents: any[] = [];
   
-//     // before all the tests are run, run this chunk of code once
-//     beforeAll(() => {
-//       factory = new FakeDataFactory;
+    // before all the tests are run, run this chunk of code once
+    beforeAll(() => {
+        factory = new FakeDataFactory;
       
-//       // Create random user, NOT stored in database
-//       user = factory.randomUser({ id: "TEST_USER" });
+        // Create random user, NOT stored in database
+        user = factory.randomUser({ id: "TEST_USER" });
   
-//       // Create random preference, NOT stored in database
-//       preference = factory.randomUserPreference({ userId: "TEST_USER_PREFERENCE" });
+        // Create random event, NOT stored in database
+        userEvent = factory.randomUserEvent({ id: "TEST_USER_EVENT", userId: "TEST_USER" });
+
+        // Create random events, NOT stored in database
+        for (let i = 0; i < 10; i++) {
+            userEvents.push(factory.randomUserEvent());
+            }
+        });
     
-//       // Create random preference question, NOT stored in database
-//       preferenceQuestion = factory.randomPreferenceQuestion({ id: "TEST_PREFERENCE_QUESTION"});
-  
-//       // Create random questions, NOT stored in database
-//       for (let i = 0; i < 10; i++) {
-//         preferenceQuestions.push(factory.randomPreferenceQuestion());
-//       }
-//     });
     
-//     // describe refers to a group of tests
-//     describe('Test getting preference routes', async () => {
-//       let preferences;
-//       let dbPreferences: any[];
+    // describe refers to a group of tests
+    describe('Test getting events', async () => {
+      let events;
+      let dbEvents: any[];
   
-//       // add data to the database which lets us test the routes
-//       beforeAll(async () => {
-//         // Add some answers to preference questions
-//         preferences = preferenceQuestions.map(question =>
-//           factory.randomUserPreference({
-//             id: false,
-//             userId: user.id,
-//             questionId: question.id,
-//           }));
-//         // Add answers to database
-//         dbPreferences = await prisma.user_preferences.createManyAndReturn({
-//           data: preferences,
+      // add data to the database which lets us test the routes
+      beforeAll(async () => {
+        // Map generated user events to their user ID
+        events = userEvents.map(event =>
+          factory.randomUserEvent({
+            id: false,
+            userId: user.id,
+          }));
+        // Add events to database
+        dbEvents = await prisma.user_events.createManyAndReturn({
+            data: events,
+          });
+      });
+  
+      // Test getting the events through the route
+      it('Gets the events', async () => {
+        const res = await request(app).get(`/api/calendar/events/by-user/${user.id}`);
+        expect(res.statusCode).toBe(200);
+      });
+
+      // Comment this out if you don't want things to delete
+      // Delete answers when done
+      afterAll(async () => {
+        await prisma.user_events.deleteMany({
+          where: {
+            user_id: "TEST_USER",
+          },
+        });
+      });
+
+    });
+
+        // describe refers to a group of tests
+    describe('Test getting events', async () => {
+      let events;
+      let dbEvents: any[];
+  
+      // add data to the database which lets us test the routes
+      beforeAll(async () => {
+        // Map generated user events to their user ID
+        events = userEvents.map(event =>
+          factory.randomUserEvent({
+            id: false,
+            userId: user.id,
+          }));
+        // Add events to database
+        dbEvents = await prisma.user_events.createManyAndReturn({
+            data: events,
+          });
+      });
+  
+      // Test getting the events through the route
+      it('Gets the events', async () => {
+        const res = await request(app).get(`/api/calendar/events/by-user/${user.id}`);
+        expect(res.statusCode).toBe(200);
+      });
+
+      // Comment this out if you don't want things to delete
+      // Delete answers when done
+      afterAll(async () => {
+        await prisma.user_events.deleteMany({
+          where: {
+            user_id: "TEST_USER",
+          },
+        });
+      });
+
+    });
+
+    // describe refers to a group of tests
+    describe('Test getting an event by id', async () => {
+        let dbEvent: any;
+    
+        // add data to the database which lets us test the routes
+        beforeAll(async () => {
+          // Add an event to database
+          dbEvent = await prisma.user_events.create({
+              data: {
+                id: 'TEST_USER_EVENT',
+                user_id: 'TEST_USER',
+                title: 'TEST_TITLE'
+              }
+            });
+        });
+    
+        // Test getting the event through its id using the route
+        it('Gets the events', async () => {
+          const res = await request(app).get(`/api/calendar/events/id/${userEvent.id}`);
+          expect(res.statusCode).toBe(200);
+        });
+  
+        // Comment this out if you don't want things to delete
+        // Delete answers when done
+        afterAll(async () => {
+          await prisma.user_events.deleteMany({
+            where: {
+              id: "TEST_USER_EVENT",
+            },
+          });
+        });
+  
+      });
+
+//    // describe refers to a group of tests
+//    describe('Test updating an event', async () => {
+//     let dbEvent: any;
+
+//     // add data to the database which lets us test the routes
+//     beforeAll(async () => {
+//       // Add an event to database
+//       dbEvent = await prisma.user_events.create({
+//           data: {
+//             id: 'TEST_USER_EVENT',
+//             user_id: 'TEST_USER',
+//             title: 'TEST_TITLE'
+//           }
 //         });
-//       });
-  
-//       // Comment this out if you don't want things to delete
-//       // Delete answers when done
-//       afterAll(async () => {
-//         await prisma.user_preferences.deleteMany({
-//           where: {
-//             user_id: "TEST_USER",
-//           },
-//         });
-//       });
-  
-//       it('Gets the preferences', async () => {
-//         const res = await request(app).get(`/api/user/surveyresults/${user.id}`);
-//         expect(res.statusCode).toBe(200);
-//       });
 //     });
-  
-  
+
+//     // Test getting the event through its id using the route
+//     it('Updates an event', async () => {
+      
+//       let testEvent;
+//       testEvent = {
+//         id: 'TEST_USER_EVENT',
+//         // title: 'NEW_TEST_TITLE'
+//       };
+//       const res = await request(app)
+//       .put(`/api/calendar/events/id/${userEvent.id}`)
+//     //   .send([testEvent]);
+//       expect(res.statusCode).toBe(200);
+//     });
+
+//     // Comment this out if you don't want things to delete
+//     // Delete answers when done
+//     // afterAll(async () => {
+//     //   await prisma.user_events.deleteMany({
+//     //     where: {
+//     //       id: "TEST_USER_EVENT",
+//     //     },
+//     //   });
+//     // });
+
 //   });
+  
+  });
