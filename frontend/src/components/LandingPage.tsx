1b68bbeff54f8a93b19f686605345522c9ed40f2
khanhdo05 <khanhphuongdo28@gmail.com>
2025-04-25 13:14:46 -0500

    FIX LINT

--- frontend/src/components/LandingPage.tsx
+++ frontend/src/components/LandingPage.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { React, useEffect } from 'react';
+import React, { useEffect } from 'react';
 // import { useRouter } from 'next/navigation';
 import { motion } from 'framer-motion';
 import { Calendar } from 'lucide-react';
