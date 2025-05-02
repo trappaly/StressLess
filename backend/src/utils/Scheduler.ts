e3b70ecf90d192ab356ddd94e597b54d399634a0
Tim Yu <yuyu.tim@gmail.com>
2025-05-02 10:38:36 -0500

    Added some code to scheduler for finding next free time block

--- backend/src/utils/Scheduler.ts
+++ backend/src/utils/Scheduler.ts
@@ -2,17 +2,25 @@ import UserPreferences from "./UserPreferences";
 import { Prisma } from "@prisma/client";
 
 /**
+ * An object containing a deadline object as well as the number of unscheduled minutes.
+ */
+type DeadlineRemainder = {
+  deadline: Prisma.user_deadlinesCreateInput,
+  unscheduledMinutes: number,
+}
+
+/**
  * The output of the scheduler.
  * 
- * scheduledEvents is an array of events needed to finish a deadline.
+ * `scheduledEvents` is an array of events needed to finish a deadline.
  * 
- * unscheduledDeadlines is an array of deadlines that are not fully scheduled.
- * unscheduledDeadlines will contain the number of hours that remain unscheduled,
+ * `unscheduledDeadlines` is an array of deadlines that are not fully scheduled.
+ * `unscheduledDeadlines` is an array  contain the number of hours that remain unscheduled,
  * in addition to all fields of a deadline object.
  */
-type SchedulerOutput =  {
+type SchedulerOutput = {
   scheduledEvents: Prisma.user_eventsCreateInput[],
-  unscheduledDeadlines: Prisma.user_deadlinesCreateInput[],
+  unscheduledDeadlines: DeadlineRemainder[],
 }
 
 export default class Scheduler {
@@ -26,10 +34,79 @@ export default class Scheduler {
    * @returns A SchedulerOutput object.
    */
   static scheduleDeadlines(
-    events: any[],
-    deadlines: any[],
+    events: Prisma.user_eventsCreateInput[],
+    deadlines: Prisma.user_deadlinesCreateInput[],
     userPreferences: UserPreferences
   ): SchedulerOutput {
-    // TBD
+    return {
+      scheduledEvents: [],
+      unscheduledDeadlines: [],
+    };
+  }
+
+  /**
+   * Calculates the remaining hours left for each deadline.
+   * @param events An array of all event objects for a user.
+   * @param deadlines An array of all deadline objects for a user.
+   * @returns An array of deadline remainders, the deadline and the unscheduled minutes.
+   */
+  static calculateDeadlineRemainders(
+    events: Prisma.user_eventsCreateInput[],
+    deadlines: Prisma.user_deadlinesCreateInput[],
+  ): DeadlineRemainder[] {
+    return [];
+  }
+
+  /**
+   * Returns the next free time block where a user is available for working.
+   * Search bounded by end of work day.
+   * 
+   * @param events An array of all event objects for a user.
+   * @param userPreferences The user preferences.
+   * @param startSearchTime The start time for searching the next free time block.
+   * @returns an object containing the time and date for the next free time block;
+   *  Returns 
+   */
+  static nextFreeTimeBlock(
+    events: Prisma.user_eventsCreateInput[],
+    userPreferences: UserPreferences,
+    startSearchTime: Date = new Date(Date.now()),
+  ): { time: Date, minutes: number } | null {
+    // Determine if current time is free
+    const startTimeIsFree = this.timeIsFree(events, userPreferences, startSearchTime); 
+    if (startTimeIsFree) {
+      // If start time is free, then check how soon until next event.
+      const availableMinutes = this.minutesToNextEvent(events, userPreferences, startSearchTime);
+      return {
+        time: startSearchTime,
+        minutes: availableMinutes < userPreferences.workDuration ? availableMinutes : userPreferences.workDuration,
+      };
+    } else {
+      // Otherwise, check when is the next time block.
+    }
+    return null;
+  }
+
+  /**
+   * Check if current time is free.
+   */
+  private static timeIsFree(
+    events: Prisma.user_eventsCreateInput[],
+    userPreferences: UserPreferences,
+    time: Date = new Date(Date.now()),
+  ): boolean {
+    return true;
+  }
+
+  /**
+   * Return the duration until the next event, or end of work day, in minutes,
+   * from the given time.
+   */
+  private static minutesToNextEvent(
+    events: Prisma.user_eventsCreateInput[],
+    userPreferences: UserPreferences,
+    time: Date = new Date(Date.now()),
+  ): number {
+    return 0;
   }
 }
